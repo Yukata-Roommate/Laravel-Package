e34a5a0ec30e7b947106be250cc1cf09871abaf3
Yukata Roommate <info@yukata-roommate.com>
2025-03-18 08:17:20 +0900

    20250318

--- configs/package.php
+++ configs/package.php
@@ -2,15 +2,6 @@
 
 return [
     /*========================================*
-     * Request
-     *========================================*/
-
-    "request" => [
-        "unauthorized_message"     => env("YR_REQUEST_UNAUTHORIZED_MESSAGE", ""),
-        "unauthorized_message_key" => env("YR_REQUEST_UNAUTHORIZED_MESSAGE_KEY", ""),
-    ],
-
-    /*========================================*
      * User
      *========================================*/
 
