b22aae9f0b7a3748316cb7cee1324d87cc33d4de
Yukata Roommate <info@yukata-roommate.com>
2025-03-18 06:53:29 +0900

    20250318

--- configs/package.php
+++ configs/package.php
@@ -2,38 +2,6 @@
 
 return [
     /*========================================*
-     * Exception
-     *========================================*/
-
-    "exception" => [
-        "logging" => [
-            "enable" => env("YR_EXCEPTION_LOGGING_ENABLE", false),
-
-            "base_directory" => env("YR_EXCEPTION_LOGGING_BASE_DIRECTORY", storage_path("logs")),
-            "directory"      => env("YR_EXCEPTION_LOGGING_DIRECTORY", "exception"),
-            "file"           => [
-                "name_format" => env("YR_EXCEPTION_LOGGING_FILE_NAME_FORMAT", "Y-m-d"),
-                "extension"   => env("YR_EXCEPTION_LOGGING_FILE_EXTENSION", "log"),
-            ],
-        ],
-
-        "mailing" => [
-            "enable" => env("YR_EXCEPTION_MAILING_ENABLE", false),
-
-            "subject" => env("YR_EXCEPTION_MAILING_SUBJECT", "Exception Occurred"),
-
-            "from" => [
-                "address" => env("YR_EXCEPTION_MAILING_FROM_ADDRESS", null),
-                "name"    => env("YR_EXCEPTION_MAILING_FROM_NAME", null),
-            ],
-
-            "to" => [
-                env("YR_EXCEPTION_MAILING_TO_NAME", "") => env("YR_EXCEPTION_MAILING_TO_ADDRESS", ""),
-            ],
-        ],
-    ],
-
-    /*========================================*
      * Request
      *========================================*/
 
